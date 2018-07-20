83b9654ac62721ec62339d728952fe7dead64ce6
Kalvin Lam <kalvin.lam5@gmail.com>
2018-07-20 10:14:21 -0400

    Updating eboard page

--- eboard.php
+++ eboard.php
@@ -8,111 +8,79 @@
 							<!-- Content -->
 								<article>
 
-									<h2>Executive Board 2017-18</h2>
+									<h2>Executive Board 2018-19</h2>
 
 									<table>
 									<tr>
 									<td width="50%" style="padding-right:20px; padding-bottom:20px;">
 									<img src="images/eboard/17-18/kim.jpg" height="160" width="160" style="float:left; padding-right:20px; padding-bottom:20px">
-									<h3>Kim Pham '19</h3>
-									<h4>President</h4>
-									<p>Hello! My name is Kim and I am a junior studying Materials Chemistry. If you’re wondering where I am most of the time on campus, you can spot me at Geochem in the NMR room or in a ballroom class at one of the auditoriums. Time permitting, I knit/crochet, hike (if an occasion were to come up) and cook Xoi (sticky rice). I’m super excited to be your Co-Executive Chair after spending two years on the E-Board and am looking forward to making this year a memorable one!</p>
+									<h3>Harrison Tran '20</h3>
+									<h4>Co-President</h4>
+									<p>Hi! My name is Harrison and I am an incoming junior from San Jose, California. I am a Neuroscience concentrator and a pre-med. When I have free time, I hit the Nelson, play basketball, socialize at Vivi’s or play board games. I’m also a huge foodie (favs: Ken’s Ramen, Pho Horns) and enjoy the occasional anime series (favs: HxH).</p>
 									</td>
 
 									<td width="50%" style="padding-bottom:20px;">
 									<img src="images/eboard/17-18/harrison.jpg" height="160" width="160" style="float:left; padding-right:20px; padding-bottom:20px">
-									<h3>Harrison Tran '20</h3>
-									<h4>Vice President</h4>
-									<p>Hi! My name is Harrison and I am an incoming Sophomore from San Jose, California. I plan to concentrate in Neuroscience as a pre-med. On my free time, I enjoy writing, playing basketball at the OMAC, watching TV (all time favorite series include AoT and Friends :^)) and catching up with friends. Also, I am a huge foodie who is always down to find or cook (*try to*) new foods!</p>
+									<h3>Kalvin Lam '20</h3>
+									<h4>Co-President</h4>
+									<p>Hey you! Yeah, you! :) Join VSA! It's awethum, fun, and you will get to meet awethum people (like me, hehehehe (jk) ). But yeah, I'm Kalvin, I'm a CS major, I love to play sports (soccer, bball, run), and I love alpacas (and llamas). I'm from San Jose, CA, and I am a huge BOBA FANATIC!! Anywho, hope to see you around!</p>
 									</td>
 									</tr>
 
 									<tr>
 									<td width="50%" style="padding-right:20px; padding-bottom:20px;">
 									<img src="images/eboard/17-18/connor.jpg" height="160" width="160" style="float:left; padding-right:20px; padding-bottom:15px">
-									<h3>Connor Walsh '20</h3>
-									<h4>External Affairs Chair</h4>
-									<p>Hi, I'm Connor. I'm a sophomore studying illustration at RISD. I'm interested in many areas of visual art but I primarily enjoy drawing and painting. Art is basically my life now, but I was fortunate to join VSA, which is a wholesome and welcome breather from the often engrossing nature of my practice. I grew up in Honolulu, HI, so Providence is a big change for me but I'm enjoying myself so far.</p>
+									<h3>Malery Nguyen '21</h3>
+									<h4>Vice President</h4>
+									<p>Hey y’all, I’m from Tampa, Florida, concentrating in who-knows-what, and I love my family (VSA included). I get excited about architecture, blueberries, environmental science, peaches, engineering, artichokes, biology, fishing, and art. I've found a very reassuring sense of home at Brown because of the people in VSA, and hope to share this comforting feeling with others by warmly welcoming you with open arms (and yummy food!) to our family. (๑•ᴗ•)</p>
 									</td>
-									
+
 									<td width="50%" style="padding-bottom:20px;">
 									<img src="images/eboard/17-18/jennifer.jpg" height="160" width="160" style="float:left; padding-right:20px; padding-bottom:20px">
-									<h3>Jennifer Bui '20</h3>
-									<h4>Culture Chair</h4>
-									<p>Wey Hey! Welcome to VSA! I am a sophomore and engineering concentrator. I was born in Worcester, MA to two Vietnamese immigrants #firstgen. I currently live in San Diego, CA. The love of my life is my pupper Terry. He’s 4 years old! My favorite foods are fruits: mango, sầu riêng (durian), orange, nectarine, and hồng (persimmon). I am very enthusiastic, energetic, and hella snazzy, hence my “Most Extra” award from the VSA Eboard. In my free time, I love to treat myself with films and sweets. Y’all can catch me prancing with the VSA Dance Troupe making unnecessary purchases from CVS. My life goals are to take care of my family, inspire young people to pursue higher education, and become the greatest engineer. XOXOX JeNniFEr BUi</p>
+									<h3>Melanie Tran '20</h3>
+									<h4>Secretary</h4>
+									<p>Hey! My name is Melanie, and I am a junior from San Jose, CA. I currently concentrate in Biology on the Immunobio track. On most days when I’m not running around campus, you can probably find me in the SciLi basement or at the Ratty. Outside of VSA, I volunteer with the STEMS program and mentor first-year students in the WiSE program. In my free time, I like to listen to music, watch TV (current favorite is Ali Wong: Baby Cobra), and get boba (ever heard of Happy Lemon?). If you ever wanna chat, just say hi! :)</p>
 									</td>
 									</tr>
 
 									<tr>
 									<td width="50%" style="padding-right:20px; padding-bottom:20px;">
 									<img src="images/eboard/17-18/alyssa.jpg" height="160" width="160" style="float:left; padding-right:20px; padding-bottom:15px">
-									<h3>Alyssa Pho '20</h3>
-									<h4>Family Chair</h4>
-									<p>Hi! My name's Alyssa, a sophomore from Southern California. When not rushing from class to class ...or to one of the bazillion clubs I'm in (VSA, Daebak, BEAM, BTS...did I mention VSA?), I can be found whipping something up to eat or lounging around reading light novels.  Previously a part of VSA as a First-Year Representative, I’m super excited to be your Family Chair this year! As Family Chair, I help plan events and our Anh Chị Em Program. I hope you'll join VSA as we invite you to relax a little, have some fun, and join our family <3</p>
+									<h3>Huy Tran '21</h3>
+									<h4>Publicity Chair</h4>
+									<p>Hello, my name is Huy and I am a sophomore who intends to concentrate in Biology in order to fulfill the pre-dental track requirements. Outside of class, I enjoy spending time with friends, grabbing a bite to eat, or simply listening to music. I also love the outdoors and have gone on multiple hiking, backpacking, and camping trips. Feel free to introduce yourselves when you get the chance!</p>
 									</td>
-									
+
 									<td width="50%" style="padding-bottom:20px;">
 									<img src="images/eboard/17-18/jae.jpg" height="160" width="160" style="float:left; padding-right:20px; padding-bottom:20px">
-									<h3>Jae Kim '20</h3>
-									<h4>Social Chair</h4>
-									<p>Hey, my name's Jae and I'm from London, UK. I'm a Sophomore studying Classics and Economics. Along with Kalvin, I'm one of the Social Chairs this year. At Brown, I'm  also on the rugby team (BRFC), a Meiklejohn, and in Buxton International House. Aside from those, I listen to a lot of jazz and funk, I like dogs, and my favourite colour is blue.</p>
+									<h3>Dalena Nguyen '20</h3>
+									<h4>Family Chair</h4>
+									<p>Hey there guys! My name is Dalena and I’m from Boston, MA. I’m currently a junior concentrating in Classics on the Latin track (and maybe Education Studies too). You can find me at most of the VSA events that we have. Or, if you’re on meal plan or have a swipe daddy you’ll usually find me working at VDub as a BUDS worker. Besides being part of the VSA e-board I also do traditional and modern Vietnamese dances like fan and hat dance on campus [you should see our shows and become our fan ;)]. I love anime and Pokemon, so feel free to chat with me about them (especially My Hero Academia!) over some boba or Poke bowls! ^_^</p>
 									</td>
 									</tr>
 
 									<tr>
 									<td width="50%" style="padding-right:20px; padding-bottom:20px;">
 									<img src="images/eboard/17-18/kalvin.jpg" height="160" width="160" style="float:left; padding-right:20px; padding-bottom:15px">
-									<h3>Kalvin Lam '20</h3>
-									<h4>Social Chair</h4>
-									<p>Hey you! Yeah, you! :) Join VSA! It's awethum, fun, and you will get to meet awethum people (like me, hehehehe (jk) ). But yeah, I'm Kalvin, I'm a CS major, I love to play sports (soccer, bball, run), and I love alpacas (and llamas). I'm from San Jose, CA, and I am a huge BOBA FANATIC!! Anywho, hope to see you around!</p>
+									<h3>Vivian Van '21</h3>
+									<h4>Culture Chair</h4>
+									<p>Why hello there! I am a sophomore concentrating in bio (I think) and am from good ole M-I-S-S-I-S-S-I-P-P-I. My interests center around eating/cooking food ~especially at VSA events ;) ~, nature, ASL, and watching random YouTube videos. If you can’t find me, I am probably in class, at work (come say hi at Shiru!), attending VSA events, exploring random places, helping out at a SPEC event, or at Jo’s. I’m excited to be this year’s culture chair! Pleaseee feel free to chat with me about what you want to focus on or learn more about on the oh so amazing Vietnamese culture!</p>
 									</td>
-									
+
 									<td width="50%" style="padding-bottom:20px;">
 									<img src="images/eboard/17-18/elizabeth.jpg" height="160" width="160" style="float:left; padding-right:20px; padding-bottom:20px">
-									<h3>Elizabeth Tran '20</h3>
-									<h4>EB Member at Large</h4>
-									<p>Hi! My name is Elizabeth, and I'm a sophomore concentrating in Public Policy. I'm from the San Francisco Bay Area in California. As an EB member at large, I help the rest of the Executive Board with planning and organizing VSA events. On campus I'm also a part of the Brown Pre-Law Society and HeForShe. My favorite Vietnamese dishes are cơm tấm (broken rice) and hủ tiếu (a type of noodle soup). I'm excited to see where this year takes us in VSA, and I hope to see you at our general body meetings!</p>
+									<h3>Dennis Chau '20</h3>
+									<h4>Social Chair</h4>
+									<p>Ayeee! What’s good y’all! My name is Dennis Chau and I am a Junior from Hayward, California. I have much love for taro milk-tea, burritos, my momma, and Frank Ocean’s voice that makes me sweat through my eyes for some reason. On rare occasion, you might find me at the Nelson gym because I enjoy taking care of my mind and body via repeatedly liftings weights and putting them back in the same place. As an aspiring psychology major, I make it my business to be there for others (as I hope you’ll be there for me too!) and to make people laugh because I love seeing y’all smile (yes you, cutie!). Coming from one of the most diverse cities in America, I have learned to see the beauty in each culture and life has shown me that it is possible for seemingly different people to struggle & thrive together as a community, proving we might not be so different after all (anyone can join #VSAfam!). As your social chair, I humbly ask y’all not to be afraid of approaching me and saying ‘hi’ or ‘hey’ or ‘sup qt’ because even though -at times- I may seem confident or social, I’m actually a very shy guy.</p>
 									</td>
 									</tr>
 
 									<tr>
 									<td width="50%" style="padding-right:20px; padding-bottom:20px;">
 									<img src="images/eboard/17-18/melanie.jpg" height="160" width="160" style="float:left; padding-right:20px; padding-bottom:15px">
-									<h3>Melanie Tran '20</h3>
-									<h4>EB Member at Large</h4>
-									<p>Hey! My name is Melanie, and I’m a sophomore studying Biology. I am an EB member at large. I’m from San Jose, California. Some of my life goals include purchasing a Kitchen Aid mixer, owning a pug, and becoming a pediatrician. I hope to learn how to cook my top 5 Viet dishes someday: Bánh Tôm, Hủ Tiếu, Chả Cá Lã Vọng, Nem Nướng Cuốn, and Mì Quảng. At Brown, I participate in WiSE (Women in Science & Engineering, STEMS (Swearer Tutoring and Enrichment in Math and Sciences, CareFree Clinic, and of course, VSA!</p>
-									</td>
-
-									<td width="50%" style="padding-bottom:20px;">
-									<img src="images/eboard/17-18/augustus.jpg" height="160" width="160" style="float:left; padding-right:20px; padding-bottom:20px">
-									<h3>Augustus Seabrooke '17.5</h3>
-									<h4>EB Member at Large</h4>
-									<p>My name is Augustus. I'm a biology concentrator, a .5er- graduating in December 2017, and I'm passionate about promoting culture and inclusion in communities. You may see me working at the lotus pepper food truck while learning about Vietnamese cuisine from the Lotus Pepper food truck lady. I'm super excited to be on Brown's VSA E-board, and I can't wait for new faces from the class of 2021 to show up and show out to Brown's VSA events. It's gonna be a banger.</p>
-									</td>
-									</tr>
-
-									<tr>
-									<td width="50%" style="padding-right:20px; padding-bottom:20px;">
-									<img src="images/eboard/17-18/kimberly.jpg" height="160" width="160" style="float:left; padding-right:20px; padding-bottom:15px">
-									<h3>Kimberly Pham '18</h3>
-									<h4>EB Member at Large</h4>
-									<p>Hello! My name is Kimberly and I am a senior concentrating in Health and Human Biology. I am from Los Angeles, California (yes, I do miss the great food and warmer weather). In my free time, I like to cook and bake, as well as adventure around Rhode Island when I have time. I am excited to be a part of this year's executive board and hope to do the best that I can as an EB Member at Large.</p>
-									</td>
-									
-									<td width="50%" style="padding-right:20px;">
-									<img src="images/eboard/17-18/arthur.jpg" height="160" width="160" style="float:left; padding-right:20px; padding-bottom:20px">
-									<h3>Arthur Tran '18</h3>
+									<h3>Kim Pham '20</h3>
 									<h4>Senior Advisor</h4>
-									<p>Hey there! I'm Arthur, a senior from Orange County, CA studying Economics and Health & Human Biology. Last year, I was Co-President of VSA and named "Most Likely to be the World's Next Dictator" by the E-Board. (That's actually my fallback if I can't find a job after graduation.) This year, I'm serving as one of your Senior Advisors. On campus, I advise first-years as  a Meiklejohn, do research, and work for CIS. I enjoy cooking and eating food, though I'd stop short of calling myself a foodie. I long for warmer days and bánh mì in Providence.</p>
-									</td>
-									</tr>
-
-									<tr>
-									<td width="50%">
-									<img src="images/eboard/17-18/brandon.jpg" height="160" width="160" style="float:left; padding-right:20px; padding-bottom:15px">
-									<h3>Brandon Le '18</h3>
-									<h4>Senior Advisor</h4>
-									<p>Hey VSA! My name is Brandon and I've been part of VSA for the past three years, taking on the role of Senior Advisor for my final year here! I'm a senior concentrating in Computational Biology, the intersection of software/algorithm development and biological research. Between staying up late with friends to study and making late-nite runs to Jo's, along with sledding down College Hill during wintertime and exploring our campus to find secret study spots, Brown is quite the experience! In my free time, I enjoy eating bowls of pho, producing and curating memes, EDM music, and video games.</p>
+									<p>Hey there! My name is Kim and I am a senior from Boston studying Materials Chemistry. You can spot me doing research at Geochem 90% of the time, with the other 10% being at a VSA event :). Time permitting, I knit/crochet/embroider, hike (if an occasion were to come up), cook, and go to ballroom socials! I will be serving as Senior advisor this year, and have previously been President, Vice President, and Freshrep. I love grabbing boba and food with people so I’m super excited to make trips with old and new friends this year!</p>
 									</td>
 									</tr>
 									</table>
